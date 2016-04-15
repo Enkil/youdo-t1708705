18888a5bd47dc10c374822dc54ac350cdcfc5c8e
Igor [Enkil] Timohin <timohin.i@gmail.com>
2016-04-15 03:49:57 +0300

    set devBuild check by node env

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-const gulp = require('gulp');
-const taskListing = require('gulp-task-listing');
-const requireDir = require('require-dir');
+const 
+    gulp =            require('gulp'), 
+    taskListing =     require('gulp-task-listing'), 
+    requireDir =      require('require-dir');
  
 // Add a task to render the output 
 gulp.task('list', taskListing);
 
+// Set Env
+process.env.NODE_ENV = 'development';
+
 // Check ENV
-global.devBuild = process.env.RAILS_ENV !== 'production' || process.env.RAILS_ENV !== 'staging';
+global.devBuild = process.env.NODE_ENV !== 'production';
 
 // Require all tasks in gulp/tasks, including subfolders
 requireDir('./gulp/tasks', { recurse: true });
