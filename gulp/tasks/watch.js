50c8e5ed0ff105bd65da468ef7004ddda1e7b79b
Igor [Enkil] Timohin <timohin.i@gmail.com>
2016-06-01 09:10:39 +0300

    fix watch img

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -12,7 +12,7 @@ gulp.task('watch',['browser-sync'], (cb) => {
     gulp.watch(config.css.src, ['css','styleguide']);
     gulp.watch(config.html.src + '/**/*.jade', ['html']);
     gulp.watch(config.js.src + '/**/*.js', ['js']);
-    gulp.watch(config.images.src, ['img`']);
+    gulp.watch(config.images.src, ['img']);
     gulp.watch(config.svg.src, ['svg']);
     gulp.watch(config.pngSprite.src, ['png-sprite']);
     gulp.watch(config.svgSprite.src, ['svg-sprite']);
